4486c07563666f6f9bd6fb350f56868d8e2886a0
Rafael Franco Rosa <r.rosa@dasa.com.br>
2022-04-21 17:47:56 -0300

    First commits related to infra (spring app)

---
+++ src/main/java/com/festival/tools/quicksearch/QuickSearchApplication.java
@@ -0,0 +1,13 @@
+package com.festival.tools.quicksearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/festival/tools/quicksearch/QuickSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festival.tools.quicksearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
